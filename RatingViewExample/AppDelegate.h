daa85ae72305b2bdc87bddc53924d6db391f7aeb
Selim Bakdemir <selim.bakdemir@gmail.com>
2015-10-03 21:05:18 +0300

    Initial Commit

---
+++ RatingViewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RatingViewExample
+//
+//  Created by Selim on 03.10.15.
+//  Copyright (c) 2015 Selim Bakdemir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RatingViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RatingViewExample
+//
+//  Created by Selim on 03.10.15.
+//  Copyright (c) 2015 Selim Bakdemir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
